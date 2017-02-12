7c3deb0ea42573e42af89781dd8f382aba10e786
thelocals_job_applicant <thelocals_job_applicant@protonmail.com>
2017-02-12 21:03:46 +0300

    Add additional spec for a case when there are no images on the page

--- spec/get_images_spec.rb
+++ spec/get_images_spec.rb
@@ -5,13 +5,25 @@ describe GetImages do
     expect(GetImages::VERSION).not_to be nil
   end
 
-  context '#uris_list' do
+  describe '#uris_list' do
     let(:uri) { 'http://site_with_cat_pictures.example' }
-    let(:body) { File.read("#{fixtures_path}/example.html") }
 
-    it 'returns src attributes' do
-      allow(GetImages).to receive(:get_body).with(uri).and_return(body)
-      expect(GetImages.uris_list(uri)).to eq(['image1.jpg', 'image2.gif'])
+    context 'there are images on the webpage' do
+      let(:body) { File.read("#{fixtures_path}/example_with_imgs.html") }
+
+      it 'returns src attributes' do
+        allow(GetImages).to receive(:get_body).with(uri).and_return(body)
+        expect(GetImages.uris_list(uri)).to eq(['image1.jpg', 'image2.gif'])
+      end
+    end
+
+    context 'there are no images on the webpage' do
+      let(:body) { File.read("#{fixtures_path}/example_without_imgs.html") }
+
+      it 'returns empty array' do
+        allow(GetImages).to receive(:get_body).with(uri).and_return(body)
+        expect(GetImages.uris_list(uri)).to eq([])
+      end
     end
   end
 end
